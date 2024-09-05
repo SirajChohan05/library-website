7fb9c96a1a105ece8674c154816333fbb5f3db25
SirajChohan05 <150431620+SirajChohan05@users.noreply.github.com>
2024-09-05 17:28:53 +0100

    Add files via upload

---
+++ welcome.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="welcome.css">
+    <title>Welcome</title>
+
+</head>
+<body>
+
+<ul>
+  <li><a href="borrow.php">Borrow Book</a></li>
+  <li><a href="contact.php">Contact Us</a></li>
+  <li><a href="about.html">About Us</a></li>
+  <li><a href="login.php">Logout</a></li>
+  <li><a href="index.html">Findbooks</a></li>
+  <img src="library-pic..jpg" alt="">
+
+</ul>
+
+<div class="library-img">
+ <img src="library-building.jpg" alt="">
+
+
+</div>
+
+
+<h1>Welcome to Siraj's Library</h1>
+
+<h3>Trending Books!</h3>
+
+
+
+  
+
+
+  </div>
+
+</div>
+
+<div class="books">
+ <img src="python-book.jpeg" alt="">
+ <img src="c++_book.jpeg" alt="">
+ <img src="aqa-book.jpg" alt="">
+ <img src="html-book.jpg" alt="">
+ <img src="javascript-book.jpg" alt="">
+ <img src="java-book.jpeg" alt="">
+ <img src="aqa-maths-book.jpeg" alt="">
+ <img src="aqa-french-book.jpeg" alt="">
+ 
+ 
+
+
+
+</div>
+
+ 
+
+
+
+    
+</body>
+</html>
+
